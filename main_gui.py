3df2e785925c4bd568e2ebec7b761f833c7109bf
Keith <keithathan8888@gmail.com>
2026-05-31 11:56:41 +0800

    addded buttons that changes title in main_gui

--- main_gui.py
+++ main_gui.py
@@ -1,20 +1,50 @@
-from tkinter import *
+import tkinter as tk
+import area
+import conversion
 
-window = Tk()
-window.geometry("400x550")
-window.title("Area Calculator and Unit Converter")
+def change_button_area():
+    title.config(text="Area Calculator")
 
-icon = PhotoImage(file="logo.png")
-window.iconphoto(True, icon)
-window.config(background= "black")
+def change_button_unit():
+    title.config(text="Unit Converter")
 
-label = Label(
+window = tk.Tk()
+window.title("Area and Conversion Calculator")
+window.config(bg="#1A1A24")
+window.geometry("350x500")
+window.resizable(False, False)
+
+try:
+    icon = tk.PhotoImage(file="logo.png")
+    window.iconphoto(True, icon)
+except Exception:
+    pass
+
+title = tk.Label(
     window,
-    text="Keith's Area Calculator and Unit Converter",
-    font=("Segoe UI", 18),
-    bg="black",
-    fg="white"
+    text="Area Calculator",
+    font=("Courier New", 20, "bold"),
+    fg="#E2E8F0",
+    bg="#242533",
+    relief="ridge",
+    width=16,
+    bd=7,
+    padx=30,
+    pady=15
 )
-label.pack(pady=20)
+
+title.pack(side="top",pady=(20, 15))
+
+button_area = tk.Button(
+    text="Area Calculator",
+    command=change_button_area
+)
+button_area.pack()
+
+button_unit = tk.Button(
+    text="Unit Converter",
+    command=change_button_unit
+)
+button_unit.pack(side="right")
 
 window.mainloop()
